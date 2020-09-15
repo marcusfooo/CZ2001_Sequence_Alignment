fa353c197713108d4ea055b6ef94132fe1d02f09
marcusfooo <marcusfooo98@gmail.com>
2020-09-16 02:35:15 +0800

    Added jeff.fasta and tweaked SH

--- src/src/BruteForce.java
+++ src/src/BruteForce.java
@@ -6,16 +6,18 @@ public class BruteForce {
 	public static ArrayList<Integer> BFSearch(String query, String target) {
 		int x = query.length();
 		int y = target.length();
-		
 		// Array list to store all index positions of substring
 		ArrayList<Integer> indexpos = new ArrayList<Integer>();
 		
-		int i; //i is index for target
+		int i ,j; //i is index for target, j is index for query
 		
 		final long startTime = System.nanoTime(); // Start timer
 		for (i=0; i<y-x;i++) {
-			String check = target.substring(i,i+x);
-			if (query.equals(check)) {
+			j=0;
+			while (j<x && (target.charAt(i+j) == query.charAt(j))) {
+				j++;
+			}
+			if (j==x) {
 				indexpos.add(i+1);
 			}
 		}
