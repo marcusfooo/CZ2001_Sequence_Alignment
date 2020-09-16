240f92c3dbebb82cf76af8fe3dad9ae6cd7953f7
jkrolling <65852623+jkrolling98@users.noreply.github.com>
2020-09-16 14:37:32 +0800

    Update BruteForce.java

--- src/src/BruteForce.java
+++ src/src/BruteForce.java
@@ -6,6 +6,8 @@ public class BruteForce {
 	public static ArrayList<Integer> BFSearch(String query, String target) {
 		int x = query.length();
 		int y = target.length();
+		int count_x = 0; // count of comparison
+		int count_y = 0; // count of successful comparison
 		
 		// Array list to store all index positions of substring
 		ArrayList<Integer> indexpos = new ArrayList<Integer>();
@@ -15,8 +17,16 @@ public class BruteForce {
 		final long startTime = System.nanoTime(); // Start timer
 		for (i=0; i<y-x;i++) {
 			j=0;
-			while (j<x && (target.charAt(i+j) == query.charAt(j))) {
-				j++;
+			while (j<x) {
+				if (target.charAt(i+j) == query.charAt(j)) {
+					count_x++;
+					count_y++;
+					j++;
+				}
+				else {
+					count_x++;
+					break;
+				}
 			}
 			if (j==x) {
 				indexpos.add(i+1);
@@ -25,7 +35,9 @@ public class BruteForce {
 		
 		final long endTime = System.nanoTime(); // End timer
 		final long elapsedTime = (endTime - startTime)/1000;
-		System.out.printf("Time taken: %d microseconds\n", elapsedTime);		
+		System.out.printf("Time taken: %d microseconds\n", elapsedTime);
+		System.out.printf("Total no. of comparison: %d\n", count_x);
+		System.out.printf("Total no. of successful comparison: %d\n", count_y);
 		return indexpos;
 		
 	}
